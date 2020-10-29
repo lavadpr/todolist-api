6eb2b4e5db9e8b6b332f3604ad5801b7c49e2c4a
LAVADPR <lavadpr@oocl.com>
2020-10-29 16:04:29 +0800

    [Todolist][Leo&Prince] test - should_return_todos_when_create_given_task integration test

--- src/test/java/com/oocl/todolistapi/integration/TodoItemIntegrationTest.java
+++ src/test/java/com/oocl/todolistapi/integration/TodoItemIntegrationTest.java
@@ -3,17 +3,19 @@ package com.oocl.todolistapi.integration;
 import com.oocl.todolistapi.model.TodoItem;
 import com.oocl.todolistapi.repository.TodoItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -46,11 +48,29 @@ class TodoItemIntegrationTest {
     }
 
     @Test
-    void create() {
+    void should_return_todos_when_create_given_task() throws Exception {
+        // given
+        String employeeAsJson = "{\n" +
+                "  \"text\": \"test\",\n" +
+                "  \"done\": false\n" +
+                "}";
+
+        // when
+        // then
+        mockMvc.perform(post("/todos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(employeeAsJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.text").value("test"))
+                .andExpect(jsonPath("$.done").value(false));
+
+        List<TodoItem> employees = todoItemRepository.findAll();
+        Assertions.assertEquals(1, employees.size());
     }
 
     @Test
-    void deleteTodoItem() throws Exception {
+    void should_remove_todo_when_delete_by_id() throws Exception {
         //given
         TodoItem todoItem =  todoItemRepository.save(new TodoItem("test", false));
 
